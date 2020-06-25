16c33c8f1ea994224d91e0ce2c4fe87c5fad60bb
Hawaijar.js <hawaijar.js@gmail.com>
2020-06-26 01:02:36 +0530

    Updated images and json files

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,20 +2,14 @@
 import React from 'react';
 import Helmet from 'react-helmet';
 import { withPrefix } from 'gatsby';
-import { MDXProvider } from "@mdx-js/react"
-import type { Node as ReactNode } from 'react';
 import { useSiteMetadata } from '../../hooks';
 import styles from './Layout.module.scss';
-import BinaryNode from '../BinaryNode';
-
 type Props = {
   children: ReactNode,
   title: string,
   description?: string,
   socialImage? :string
 };
-const Hello = () => <div>Hello World!</div>
-const shortcodes = { BinaryNode, Hello }
 
 const Layout = ({
   children,
@@ -40,7 +34,7 @@ const Layout = ({
         <meta name="twitter:description" content={description} />
         <meta name="twitter:image" content={metaImageUrl} />
       </Helmet>
-      <MDXProvider components={shortcodes}>{children}</MDXProvider>
+      {children}
     </div>
   );
 };
