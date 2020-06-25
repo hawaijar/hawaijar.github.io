753998160a84efff5587b46734b26bcaab9edf8f
Hawaijar.js <hawaijar.js@gmail.com>
2020-06-26 00:30:02 +0530

    Added framer module to start animation

--- src/components/Heap/Layout.jsx
+++ src/components/Heap/Layout.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import BinaryNode from '../SVG';
 import './index.scss';
 const nums = [5, 15, 3, 23, 10, 2, 50, 3, 1, 8];
+
 const Layout = () => {
     return(
         <div className='container'>
--- src/components/SVG/node.js
+++ src/components/SVG/node.js
@@ -1,28 +1,34 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { motion } from "framer-motion"
 
-const Node = ({value, position}) => {
+const {number} = PropTypes;
+
+const Node = ({value, position, animate={}}) => {
     return (
-      <svg width="60" height="100">
-        <g id="container-node">
-          <g id="node">
-            <circle className="node-circle" cx="45%" cy="45%" r="22" fill="#1f77b4" />
-            <text
-              className="node-value"
-              x={value < 10? "48%": "47%"}
-              y="45%"
-              fontSize="1.2rem"
-              fill="white"
-              textAnchor="middle"
-              alignmentBaseline="central"
-              fontFamily="'Fira Mono',monospace"
-            >
-              {value}
-            </text>
-          </g>
+      <svg width="60" height="200" viewBox="0 0  60 200">
+        <g id={`container-node-${value}`}>
+          <motion.g {...animate}>
+            <g id="node">
+              <circle className="node-circle" cx="45%" cy="45%" r="22" fill="#1f77b4" />
+              <text
+                className="node-value"
+                x={value < 10? "48%": "47%"}
+                y="45%"
+                fontSize="1.2rem"
+                fill="white"
+                textAnchor="middle"
+                alignmentBaseline="central"
+                fontFamily="'Fira Mono',monospace"
+              >
+                {value}
+              </text>
+            </g>
+          </motion.g >
           <text
             fill="black"
             x="47%"
-            y="78"
+            y="62%"
             fontSize="1rem"
             textAnchor="middle"
             alignmentBaseline="central"
@@ -35,5 +41,13 @@ const Node = ({value, position}) => {
     );
   };
   
+  Node.propTypes = {
+    value: number,
+    position: number
+  }
+  Number.defaultProps = {
+    value: 0,
+    position: 0
+  }
   export default Node;
   
